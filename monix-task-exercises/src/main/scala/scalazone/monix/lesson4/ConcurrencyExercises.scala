6655a6862a2e334c0f86bd09dbfc2122ff106050
Simon Payne <simon@honestsoftware.co.uk>
2021-11-29 14:18:12 +0000

    Lesson 4 exercises completed

--- monix-task-exercises/src/main/scala/scalazone/monix/lesson4/ConcurrencyExercises.scala
+++ monix-task-exercises/src/main/scala/scalazone/monix/lesson4/ConcurrencyExercises.scala
@@ -16,7 +16,8 @@ object ConcurrencyExercises extends App {
 
   /** Compute a sum of both tasks in parallel
     */
-  def ex1(fa: Task[Int], fb: Task[Int]): Task[Int] = ???
+  def ex1(fa: Task[Int], fb: Task[Int]): Task[Int] =
+    Task.parMap2(fa, fb)(_ + _)
 
   /** If we are executing multiple tasks concurrently, it is possible that one of them could fail.
     * The default behavior is that all other task will be canceled and the Task will return with
@@ -27,10 +28,15 @@ object ConcurrencyExercises extends App {
     *
     * Try to process all `tasks` concurrently in a way that would wait for all of them to finish and gather their results.
     */
-  def ex2(tasks: List[Task[Int]]): Task[List[Option[Int]]] = ???
+  def ex2(tasks: List[Task[Int]]): Task[List[Option[Int]]] =
+    Task.parSequence(tasks.map(_.map(Option(_)).onErrorFallbackTo(Task.now(None))))
 
   /** Modify solution to `ex2` to return `Task[(Int, Int)]`
     * where the tuple is (numberOfSuccesses, numberOfFailures).
     */
-  def ex3(tasks: List[Task[Unit]]): Task[(Int, Int)] = ???
+  def ex3(tasks: List[Task[Unit]]): Task[(Int, Int)] = {
+    Task.parSequence(tasks.map(_.redeem(_ => false, _ => true)))
+      .map(_.partition { case false => false; case true => true })
+      .map((listA, listB) => (listA.size, listB.size))
+  }
 }
